ab173a07fbb0b641d6ade74ad703b56cd22c5211
Giovani Franz <giovanifranz151@gmail.com>
2023-10-17 19:27:14 -0300

    Initial commit

---
+++ src/core/either.spec.ts
@@ -0,0 +1,23 @@
+import { Either, left, right } from './either';
+
+function doSomeThing(shouldSuccess: boolean): Either<string, number> {
+  if (!shouldSuccess) {
+    return left('error');
+  }
+
+  return right(10);
+}
+
+test('Sucesso como resultado', () => {
+  const result = doSomeThing(true);
+
+  expect(result.isRight()).toBeTruthy();
+  expect(result.isLeft()).toBeFalsy();
+});
+
+test('Erro como resultado', () => {
+  const result = doSomeThing(false);
+
+  expect(result.isLeft()).toBeTruthy();
+  expect(result.isRight()).toBeFalsy();
+});
